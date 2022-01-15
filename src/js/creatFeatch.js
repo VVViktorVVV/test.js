84cf6eccee03e5196aec1bec7da0be6d3ce6be2e
Viktor Bondar <vktrbndr@gmail.com>
2022-01-15 17:32:44 +0200

    3

--- src/js/creatFeatch.js
+++ src/js/creatFeatch.js
@@ -1,90 +1,46 @@
 const axios = require('axios');
 
-// export default class NewFetchClass {
-//   constructor() {
-//     this.searchQuery = '';
-//     this.page = 1;
-//     this.perPage = 40;
-    
-//   };
-
-//   async fetchImages() {
-//     try {
-              
-//       const response = await axios.get('https://pixabay.com/api/', {
-//           params: {
-//           key: '25005048-7b53cf58cba95630f757b4484',
-//           q: this.searchQuery,
-//           image_type: 'photo',
-//           orientation: 'horizontal',
-//           safesearch: true,
-//           per_page: this.perPage,
-//           page: this.page,
-//         },
-//       });
-//       console.log(response.data)
-//       this.incrementPage();
-      
-
-//       return response.data;
-//     }
-
-    
-    
-//     catch (error) {
-//       console.error(error);
-//     }
-//   }
-
-//   get query() {
-//     return this.searchQuery;
-//   }
-  
-//   set query(newQwery) {
-//     this.searchQuery = newQwery;
-//   }
-
-//   incrementPage() {
-//     this.page += 1;
-//   }
-
-//   resetPage() {
-//     this.page = 1;
-//   }
-
- 
-
-// }
-
-
 // const Handlebars = require("handlebars");
-
-// import {cardRender} from '../hbs/cardRender.hbs';
-
-
+// import cardRender from '../hbs/cardRender.hbs';
 
 const list = document.querySelector(".gallery");
 
 
-const KEY = '2cf91cf1fed5026ae9524dc97ad33068';
+// const KEY = '2cf91cf1fed5026ae9524dc97ad33068';
 
 async function fetchTopMovies() {
     try {
               
       const response = await axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=2cf91cf1fed5026ae9524dc97ad33068',);
-      console.log(response.data)
-      return response.data;
+      console.log(response.data.results)
+      renderImagesList(response.data.results);
+      return response.data.results;
     }
-
     
     catch (error) {
       console.error(error);
-    }
+  }
+
+
 }
   
 window.addEventListener('load', fetchTopMovies);
 
 
+function createListImagesElement({ poster_path, original_title, title}) {
+    return `<li>
+    <img
+      src='${poster_path}';
+      alt='${original_title}'
+      class='poster-img'
+    />
+    <h3 class='title'>${title}</h3>
+    `
+}
 
+function renderImagesList(array) {
+    const imageElList = array.map((element => createListImagesElement(element))).join('');
+    return list.insertAdjacentHTML('beforeend', imageElList)
+}
 
 
